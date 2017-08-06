4e213e172ef13913e208d2a5d963f8b498e58b44
Paulo Trentin <paulo@paulotrentin.com.br>
2017-08-06 19:22:54 -0300

    Criado gerador de código de barras e versão de controle do robô com sensores de passo nas rodas

--- robot.ino
+++ robot.ino
@@ -1,193 +1,101 @@
-#include <Servo.h>
-Servo myservo;
+int entireRoundStep = 125;
+int enginePower = 100;
+int stepsPerAction = 40;
 
-String getValue(String, char, int);
-int getNotebookStatus ();
-void powerUpNotebook ();
-String leStringSerial1();
 
-int servoValue = 80;
-int powerUpNotebookPin = 23;
-int notebookSensor = A0;
+const int sensorRightPin = A0;
+const int sensorDirMiddleValue = 300;
+int sensorRightLastState = 0;
+int sensorRightStepCount = 0;
 
-void setup() {
-  pinMode(powerUpNotebookPin, OUTPUT);
-  pinMode(notebookSensor, INPUT);
+const int sensorLeftPin = A1;
+const int sensorLeftMiddleValue = 350;
+int sensorLeftLastState = 0;
+int sensorLeftStepCount = 0;
 
-  myservo.attach(12);
 
-  myservo.write(servoValue);
-  delay(2000);
-  //myservo.write(145);
-  delay(2000);
-  Serial.begin(9600);
-  Serial1.begin(115200);
-  Serial.println("ok");
+
+
+int xAxisData;
+int yAxisData;
+
+int leftBridge_PNP_A = 4;
+int leftBridge_NPN_A = 5;
+int leftBridge_PNP_B = 7;
+int leftBridge_NPN_B = 6;
+
+int rightBridge_PNP_A = 8;
+int rightBridge_NPN_A = 9;
+int rightBridge_PNP_B = 11;
+int rightBridge_NPN_B = 10;
+
+int xAxis = 2;
+int yAxis = 3;
+
+bool serialCommandInitialized = false;
+
+
+
+
+void proccessLeftMotor();
+void proccessRightMotor();
+
+
+
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("Inicio ok");
+  
+  
+  pinMode(xAxis, INPUT);
+  pinMode(yAxis, INPUT);
+
+  pinMode(leftBridge_PNP_A, OUTPUT);
+  pinMode(leftBridge_NPN_A, OUTPUT);
+  pinMode(leftBridge_PNP_B, OUTPUT);
+  pinMode(leftBridge_NPN_B, OUTPUT);
+  
+  pinMode(rightBridge_PNP_A, OUTPUT);
+  pinMode(rightBridge_NPN_A, OUTPUT);
+  pinMode(rightBridge_PNP_B, OUTPUT);
+  pinMode(rightBridge_NPN_B, OUTPUT);
 }
 
-void freeSerial1(){
-	while(Serial1.available() > 0){
-		Serial1.read();
-	}
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
- 
-
-  if (Serial1.available() > 0){
-    	String recebido = leStringSerial1();
-	
-      if (recebido.indexOf("MOVE_WEBCAM") != -1){
-        String desiredPosition = getValue(recebido, ':', 1);
-        
-        Serial.print("movimentar camera: ");
-        int value = desiredPosition.toInt();
-        Serial.println(value);
-        int convertedValue = map(value, 0,100,145,80);
-       
-
-	myservo.write(convertedValue); 
-	/*
-        // deixa mais suave a mudança do servo
-        if (convertedValue > servoValue){
-            for (int i = servoValue; i >= convertedValue; i++){
-                myservo.write(i);
-                delay(100);
-            }
-        }else{
-            for (int i = servoValue; i <= convertedValue; i--){
-                myservo.write(i);
-                delay(100);
-            }
-        }
-        */
-        servoValue = convertedValue;
-
-        //Serial1.print("Recebido: ");
-        //Serial1.println(recebido);
-      }else
-
-    	if (recebido == "LIGAR_NOTEBOOK"){
-    		if (getNotebookStatus()){
-    			Serial1.println("JA_LIGADO");
-    		}else{
-    			powerUpNotebook();
-    			Serial1.println("LIGANDO_NOTEBOOK");
-    		}
-
-		powerUpNotebook();
-    	}
-
-      else{
-        Serial1.print("Recebido: ");
-        Serial1.println(recebido);
-      }
-  }
- 
-  int notebookSensorValue = getNotebookStatus();
-
-  Serial1.print("note status: ");
-  Serial1.println(notebookSensorValue);
-
-  for (int i = 80; i <= 150; i+= 1){
-	//myservo.write(i);
-	//delay(50);
-  }
-
-  //digitalWrite(powerUpNotebook, HIGH);
-  //delay(1000);
-  //digitalWrite(powerUpNotebook, LOW);
-  //delay(1000);
-
-  delay(100);
-  //Serial1.println("aa");
- 
-  for (int i = 150; i >= 80; i-= 1){
-        //myservo.write(i);
-        //delay(50);
-  }
-}
-
-void powerUpNotebook(){
-  digitalWrite(powerUpNotebookPin, HIGH);
-  delay(300);
-  digitalWrite(powerUpNotebookPin, LOW);
+void loop(){
+  readStepSensors();
+  processSerialCommand();
+  
+    Serial.print(sensorLeftStepCount);
+    Serial.print(" - ");
+    Serial.println(sensorRightStepCount);
 }
 
 
 
-// Pode demorar até6 segundos para responder
-int getNotebookStatus(){
-
-  if (analogRead(notebookSensor > 100)){
-	return 1;
-  }
-
-  return 0;
-
-  int status = 0;
-  bool changed = false;
-
-  for (int i = 0; i < 60; i++){
-	int notebookSensorValue = analogRead(notebookSensor);
-	return notebookSensorValue;
-
-	if (notebookSensorValue > 300){
-		status = 1;
-	}
-
-	if ( ! notebookSensorValue){
-		if (status){
-			changed = true;
-			break;
-		}
-	}
-
-	delay(100);
-  }
-
-
-  if (changed){
-	return 2;
-  }
-
-  return status;
+void readStepSensors(){
+  proccessRightMotor();
+  proccessLeftMotor();
+  delay(10);
 }
 
 
 
-/**
- * Faz o explode e retorna um valor de acordo com o indice
- */
-String getValue(String data, char separator, int index)
-{
-    int found = 0;
-    int strIndex[] = { 0, -1 };
-    int maxIndex = data.length() - 1;
 
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
-    }
-    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
+
 
 /**
  * Função que lê uma string da Serial
  * e retorna-a
  */
-String leStringSerial1(){
+String leStringSerial(){
   String conteudo = "";
   char caractere;
   
   // Enquanto receber algo pela serial
-  while(Serial1.available() > 0) {
+  while(Serial.available() > 0) {
     // Lê byte da serial
-    caractere = Serial1.read();
+    caractere = Serial.read();
     // Ignora caractere de quebra de linha
     if (caractere != '\n'){
       // Concatena valores
@@ -196,10 +104,311 @@ String leStringSerial1(){
     // Aguarda buffer serial ler próximo caractere
     delay(10);
   }
-  
-  Serial1.flush();  
-  //Serial1.print("Recebi: ");
-  //Serial1.println(conteudo);
+    
+  Serial.print("Recebi: ");
+  Serial.println(conteudo);
     
   return conteudo;
+}
+
+
+
+
+
+void processSerialCommand(){
+
+  if (Serial.available() > 0){
+
+    serialCommandInitialized = true;
+    
+    String recebido = leStringSerial();
+
+    if (recebido == "D"){
+      goRight();
+    }
+
+    else if (recebido == "A"){
+      goLeft();
+    }
+
+    else if (recebido == "W"){
+      goFront();
+    }
+
+    else if (recebido == "S"){
+      goBack();
+    }
+
+    else {
+      stopNow();
+    }
+  }
+}
+
+
+void stopNow(){
+  disableLeftBridge();
+  disableRightBridge();
+}
+
+void goFront(){
+  goFrontWithBalancing();
+}
+
+void goBack(){
+  goBackWithBalancing();
+}
+
+void goLeft(){
+  goLeftWithBalancing();
+}
+
+void goRight(){
+  goRightWithBalancing();
+}
+
+
+
+void goLeftWithBalancing(){
+  sensorRightStepCount = 0;
+  sensorLeftStepCount = 0;
+  
+  while(sensorRightStepCount < stepsPerAction / 2){
+   
+    if (sensorRightStepCount > sensorLeftStepCount){
+      goBackLeftBridge();
+      disableRightBridge();
+    }
+    
+    else if (sensorRightStepCount < sensorLeftStepCount){
+      goFrontRightBridge();
+      disableLeftBridge();
+    } 
+    
+    else{
+      goBackLeftBridge();
+      goFrontRightBridge();
+    }
+    
+    Serial.print(sensorLeftStepCount);
+    Serial.print(" - ");
+    Serial.println(sensorRightStepCount);
+    
+    readStepSensors();
+  }
+  
+  stopNow();
+}
+
+
+
+void goRightWithBalancing(){
+  sensorRightStepCount = 0;
+  sensorLeftStepCount = 0;
+  
+  while(sensorRightStepCount < stepsPerAction / 2){
+   
+    if (sensorRightStepCount > sensorLeftStepCount){
+      goFrontLeftBridge();
+      disableRightBridge();
+    }
+    
+    else if (sensorRightStepCount < sensorLeftStepCount){
+      goBackRightBridge();
+      disableLeftBridge();
+    } 
+    
+    else{
+      goBackRightBridge();
+      goFrontLeftBridge();
+    }
+    
+    Serial.print(sensorLeftStepCount);
+    Serial.print(" - ");
+    Serial.println(sensorRightStepCount);
+    
+    readStepSensors();
+  }
+  
+  stopNow();
+}
+
+
+
+void goFrontWithBalancing(){
+  sensorRightStepCount = 0;
+  sensorLeftStepCount = 0;
+  
+  while(sensorRightStepCount < stepsPerAction){
+   
+    if (sensorRightStepCount > sensorLeftStepCount){
+      goFrontLeftBridge();
+      disableRightBridge();
+    }
+    
+    else if (sensorRightStepCount < sensorLeftStepCount){
+      goFrontRightBridge();
+      disableLeftBridge();
+    } 
+    
+    else{
+      goFrontLeftBridge();
+      goFrontRightBridge();
+    }
+    
+    Serial.print(sensorLeftStepCount);
+    Serial.print(" - ");
+    Serial.println(sensorRightStepCount);
+    
+    readStepSensors();
+  }
+  
+  stopNow();
+}
+
+void goBackWithBalancing(){
+  sensorRightStepCount = 0;
+  sensorLeftStepCount = 0;
+  
+  while(sensorRightStepCount < stepsPerAction){
+    
+    if (sensorRightStepCount == sensorLeftStepCount){
+      goBackLeftBridge();
+      goBackRightBridge();
+    }
+    
+    else if (sensorRightStepCount > sensorLeftStepCount){
+      goBackLeftBridge();
+      disableRightBridge();
+    }
+    
+    else if (sensorLeftStepCount > sensorRightStepCount){
+      goBackRightBridge();
+      disableLeftBridge();
+    }
+    
+    Serial.print(sensorLeftStepCount);
+    Serial.print(" - ");
+    Serial.println(sensorRightStepCount);
+    
+    readStepSensors();
+  }
+  
+  stopNow();
+}
+
+
+
+
+
+void disableLeftBridge(){
+  digitalWrite(leftBridge_PNP_A, LOW);
+  digitalWrite(leftBridge_NPN_A, LOW);
+  digitalWrite(leftBridge_PNP_B, LOW);
+  digitalWrite(leftBridge_NPN_B, LOW);
+}
+void goFrontLeftBridge(){
+  digitalWrite(leftBridge_PNP_A, HIGH);
+  digitalWrite(leftBridge_NPN_A, LOW);
+  digitalWrite(leftBridge_PNP_B, LOW);
+  analogWrite(leftBridge_NPN_B, enginePower);
+}
+void goBackLeftBridge(){
+  digitalWrite(leftBridge_PNP_A, LOW);
+  digitalWrite(leftBridge_NPN_A, HIGH);
+  analogWrite(leftBridge_PNP_B, enginePower);
+  digitalWrite(leftBridge_NPN_B, LOW);
+}
+
+
+void disableRightBridge(){
+  digitalWrite(rightBridge_PNP_A, LOW);
+  digitalWrite(rightBridge_NPN_A, LOW);
+  digitalWrite(rightBridge_PNP_B, LOW);
+  digitalWrite(rightBridge_NPN_B, LOW);
+}
+void goFrontRightBridge(){
+  digitalWrite(rightBridge_PNP_A, LOW);
+  digitalWrite(rightBridge_NPN_A, HIGH);
+  analogWrite(rightBridge_PNP_B, enginePower);
+  digitalWrite(rightBridge_NPN_B, LOW);
+}
+void goBackRightBridge(){
+  digitalWrite(rightBridge_PNP_A, HIGH);
+  digitalWrite(rightBridge_NPN_A, LOW);
+  digitalWrite(rightBridge_PNP_B, LOW);
+  analogWrite(rightBridge_NPN_B, enginePower);
+}
+
+
+
+
+
+
+
+void proccessRightMotor(){
+  
+  int sensorRightValue = analogRead(sensorRightPin);
+  
+  //Serial.println(sensorRightValue);
+  boolean changed = 0;
+  
+  if (sensorRightValue <= sensorDirMiddleValue){
+    if (sensorRightLastState != 0){
+      sensorRightLastState = 0;
+      changed = 1;
+    }
+  }else{
+    if (sensorRightLastState != 1){
+      sensorRightLastState = 1;
+      changed = 1;
+    }
+  }
+  
+  if (changed){
+    sensorRightStepCount++;
+    //Serial.println(sensorRightStepCount);
+
+    /*    
+    if (sensorRightStepCount == entireRoundStep){
+      Serial.println("Volta completa");
+      sensorRightStepCount = 0;
+    }
+    */
+  }
+  
+}
+
+
+void proccessLeftMotor(){
+  
+  int sensorLeftValue = analogRead(sensorLeftPin);
+  
+  //Serial.println(sensorRightValue);
+  boolean changed = 0;
+  
+  if (sensorLeftValue <= sensorLeftMiddleValue){
+    if (sensorLeftLastState != 0){
+      sensorLeftLastState = 0;
+      changed = 1;
+    }
+  }else{
+    if (sensorLeftLastState != 1){
+      sensorLeftLastState = 1;
+      changed = 1;
+    }
+  }
+  
+  if (changed){
+    sensorLeftStepCount++;
+    //Serial.println(sensorLeftStepCount);
+
+    /*    
+    if (sensorLeftStepCount == entireRoundStep){
+      Serial.println("Volta completa");
+      sensorLeftStepCount = 0;
+    }
+    */
+  }
+  
 }
