5d05f0f58df28c86feb96ddc2c7c1d6442b5cf87
Paulo Trentin <paulo@paulotrentin.com.br>
2018-03-18 00:47:28 -0300

    Criado primeira versao de conexao ponta a ponta com soma de duas webcams

---
+++ server/peerserver/server.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var PeerServer = require('peer').PeerServer;
+
+var server = PeerServer({
+	  port: 9000,
+			path: 'teste',
+	  ssl: {
+		      key: fs.readFileSync('/etc/apache2/ssl/phpit.key'),
+		      cert: fs.readFileSync('/etc/apache2/ssl/phpit.pem')
+		    }
+});
